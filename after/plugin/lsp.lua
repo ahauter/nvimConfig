bfde83b58c57a6c3d3ec8f72cf14b324b57da851
ahauter <hautera@me.com>
2023-07-14 15:50:32 -0700

    Added auto formatting config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,23 @@
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'pythonserver',
+})
+local format_opts = {
+    format_opts = {
+        async = false, 
+        timeout_ms = 5000
+    },
+    servers = {
+        ['lua_ls'] = {'lua'},
+        ['rust_analyzer'] = {'rust'},
+        ['pylsp'] = {'python'},
+        ['tsserver'] = {'javascript', 'typescript'},
+    }
+}
+lsp.format_mapping('<leader>l', format_opts)
+lsp.format_on_save(format_opts)
 lsp.setup()
