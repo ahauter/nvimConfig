9bb103f25bdbc51053c1b6bec62097b5848ac4e8
ahauter <hautera@me.com>
2023-08-10 13:11:26 -0700

    added remap for paste/nopaste

--- lua/austinSettings/remap.lua
+++ lua/austinSettings/remap.lua
@@ -1,3 +1,11 @@
 vim.g.mapleader = " "
 vim.g.ttimeoutlen = 500
 vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>vm", function()
+	if vim.o.paste then
+        vim.cmd.set("nopaste")	
+    else 
+        vim.cmd.set("paste")
+    end
+end)
+
