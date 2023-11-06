d33c4ba1f8a30da02be28dd61f8c7a4aad63b429
Austin <austinh@gritsoftwaresystems.com>
2023-11-06 07:52:56 -0800

    Added more languages to ensure installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,9 @@ lsp.preset('recommended')
 lsp.ensure_installed({
     'tsserver',
     'eslint',
-    'pythonserver',
+    'pylsp',
+    'gopls',
+    'lua_ls'
 })
 local format_opts = {
     format_opts = {
@@ -16,13 +18,13 @@ local format_opts = {
         ['rust_analyzer'] = {'rust'},
         ['pylsp'] = {'python'},
         ['tsserver'] = {'javascript', 'typescript'},
+        ['gopls'] = {'go'}
     }
 }
 lsp.format_mapping('<leader>l', format_opts)
 lsp.format_on_save(format_opts)
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap=false}
-
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
